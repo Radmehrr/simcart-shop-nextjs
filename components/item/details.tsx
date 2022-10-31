bea3a019018c011591fcb9334b3fdefeda31a8e4
radmehr <alii.radmehr98@gmail.com>
2022-10-31 05:17:33 -0700

    add phone and name og seller to simcart details

--- components/item/details.tsx
+++ components/item/details.tsx
@@ -5,14 +5,25 @@ const Details: FC<any> = ({ simcart }) => {
   return (
     <section>
       <div className="mx-3 my-5">
-        <div className="flex gap-20 mx-2 md:gap-40 dark:text-gray-700">
-          <p>شماره: </p>
+        <div className="flex gap-10 mx-2 md:gap-28 dark:text-gray-700">
+          <p>نام فروشنده:</p>
+          <p style={{ direction: "ltr" }}>{simcart.owner.fullName}</p>
+        </div>
+        <div className="flex gap-10 mx-2 my-2 md:gap-28 dark:text-gray-700">
+          <p>شماره تماس:</p>
+          <p style={{ direction: "ltr" }}>{simcart.owner.phone}</p>
+        </div>
+        <div className="flex gap-6 mx-2 md:gap-24 dark:text-gray-700">
+          <p>شماره سیمکارت:</p>
           <p style={{ direction: "ltr" }}>{simcart.phoneNumber}</p>
         </div>
 
         <div className="flex gap-20 mx-2 my-2 md:my-4 md:gap-40 dark:text-gray-700">
           <p>قیمت: </p>
-          <p>{numeral(simcart.price).format("000,000")}</p>
+          <p className="flex text-red-600">
+            {numeral(simcart.price).format("000,000")}
+            <p className="mx-1 text-black">تومان</p>
+          </p>
         </div>
 
         <div className="flex gap-16 mx-2 my-2 md:my-4 md:gap-36 dark:text-gray-700">
